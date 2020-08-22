84c6c8c2933ee262aba836287eb88b3acaf30cfa
Lucassvvargas <70048407+Lucassvvargas@users.noreply.github.com>
2020-08-22 15:05:01 -0400

    Level

--- Teaching Game/Assets/Rocket.cs
+++ Teaching Game/Assets/Rocket.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidbody;
+    [SerializeField] float thrust;
+    [SerializeField] float rotationthrust;
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("love");
+        rigidbody = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {if(Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+        }
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(-Vector3.forward * rotationthrust * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(Vector3.forward * rotationthrust * Time.deltaTime);
+            }
+        }
     }
 }
