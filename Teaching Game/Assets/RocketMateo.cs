662b65f5dabb8aadba56598c9cb5148cb74ea340
MateoBodon <55267042+MateoBodon@users.noreply.github.com>
2020-08-22 15:04:58 -0400

    Changes To Mateo

---
+++ Teaching Game/Assets/RocketMateo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMateo : MonoBehaviour
+{
+    
+    [SerializeField] float rotationThrust;
+    [SerializeField] float mainThrust;
+    Rigidbody rigidBody;
+
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationThrust * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(-Vector3.forward * rotationThrust * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+}
